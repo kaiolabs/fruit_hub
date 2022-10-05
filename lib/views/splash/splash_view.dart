64daf0225eae2b1e9b16dc8db8a9d32b7358086d
kaiolabs <peypro1@gmail.com>
2022-10-04 21:54:24 -0300

    Primeira etapa da splash concluída
    Implementação da navegação de telas e animações da logo.

---
+++ lib/views/splash/splash_view.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fruit_hub/views/home/home_view.dart';
+import 'package:fruit_hub/views/welcome/welcome_view.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  bool isLogado = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: TweenAnimationBuilder(
+            tween: Tween<double>(begin: 0, end: 1),
+            duration: const Duration(milliseconds: 600),
+            builder: (context, double value, child) {
+              return AnimatedOpacity(
+                opacity: value,
+                onEnd: () {
+                  Future.delayed(const Duration(seconds: 1), () {
+                    isLogado
+                        ? Navigator.of(context)
+                            .pushAndRemoveUntil(MaterialPageRoute(builder: (context) => const Welcome()), (Route<dynamic> route) => false)
+                        : Navigator.of(context)
+                            .pushAndRemoveUntil(MaterialPageRoute(builder: (context) => const HomeView()), (Route<dynamic> route) => false);
+                  });
+                },
+                duration: const Duration(milliseconds: 600),
+                child: Transform.scale(
+                  scale: value,
+                  child: child,
+                ),
+              );
+            },
+            child: SvgPicture.asset('assets/images/logo.svg'),
+          ),
+        ),
+      ),
+    );
+  }
+}
