3acb2f411cca2e7c1ba7456a34f3cb4139154232
kaiolabs <peypro1@gmail.com>
2022-10-05 22:48:03 -0300

    Definido a paleta de cores da aplicação

---
+++ lib/shared/palette.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/animation.dart';
+
+class Palette {
+  static const orange = Color(0xFFffa451);
+  static const lightPurple = Color(0xFF5D577E);
+  static const darkPurple = Color(0xFF27214D);
+}
