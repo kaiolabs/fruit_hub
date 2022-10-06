5de9b9cc5f746d901cc99f525ba09bcd080f8717
kaiolabs <peypro1@gmail.com>
2022-10-05 22:44:34 -0300

    Componentizando a tela de welcome

---
+++ lib/views/welcome/welcome.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'package:fruit_hub/shared/palette.dart';
+import 'package:fruit_hub/views/components/button_pattern.dart';
+
+class Welcome extends StatelessWidget {
+  final Function() onTapButton;
+  const Welcome({super.key, required this.onTapButton});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          color: const Color(0xFFffa451),
+          height: MediaQuery.of(context).size.height * 0.6,
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 50.0),
+              child: Image.asset('assets/images/Component_1.png'),
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.05),
+          child: SizedBox(
+            width: 315,
+            height: 211,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Get The Freshest Fruit Salad Combo',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Palette.darkPurple,
+                        fontFamily: 'TTNorms_Bold',
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'We deliver the best and freshest fruit salad in town. Order for a combo today!!!',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Palette.lightPurple,
+                        fontFamily: 'TTNorms_Regular',
+                      ),
+                    ),
+                  ],
+                ),
+                ButtonPattern(
+                  lebel: "Let’s Continue",
+                  onPressed: () => onTapButton(),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/views/welcome/welcome_view.dart
+++ lib/views/welcome/welcome_view.dart
@@ -1,86 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_hub/views/welcome/welcome.dart';
+import 'package:fruit_hub/views/welcome/welcome_authentication.dart';
 
 class WelcomeView extends StatelessWidget {
-  const WelcomeView({super.key});
+  WelcomeView({super.key});
+
+  final PageController _pageController = PageController(initialPage: 0);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            color: const Color(0xFFffa451),
-            height: MediaQuery.of(context).size.height * 0.6,
-            child: Center(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 50.0),
-                child: Image.asset('assets/images/Component_1.png'),
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.05),
-            child: SizedBox(
-              width: 315,
-              height: 211,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Text(
-                        'Get The Freshest Fruit Salad Combo',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Color(0xFF27214D),
-                          fontFamily: 'TTNorms_Bold',
-                        ),
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        'We deliver the best and freshest fruit salad in town. Order for a combo today!!!',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Color(0xFF5D577E),
-                          fontFamily: 'TTNorms_Regular',
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width,
-                    height: 56,
-                    child: Material(
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(10),
-                        ),
-                      ),
-                      color: const Color(0xFFffa451),
-                      child: InkWell(
-                        onTap: () {},
-                        child: const Center(
-                          child: Text(
-                            'Learn More',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontFamily: 'TTNorms_regular',
-                              color: Color(0xFFFFFFFF),
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
-    );
+        body: PageView(
+      controller: _pageController,
+      physics: const NeverScrollableScrollPhysics(),
+      children: [
+        Welcome(
+          onTapButton: () {
+            _pageController.animateToPage(
+              1,
+              duration: const Duration(milliseconds: 600),
+              curve: Curves.easeInOut,
+            );
+          },
+        ),
+        WelcomeAuthentication(),
+      ],
+    ));
   }
 }
