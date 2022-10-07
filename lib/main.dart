36c42c76a076d6a8b5cb6a4eac1a39adc41baa04
kaiolabs <peypro1@gmail.com>
2022-10-06 23:36:43 -0300

    Adicionando a parte de inicializar o db

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fruit_hub/controllers/inicializacao.dart';
+import 'package:fruit_hub/controllers/provider_welcome_authentication.dart';
 import 'package:fruit_hub/views/home/home_view.dart';
+import 'package:fruit_hub/views/splash/splash_view.dart';
+import 'package:fruit_hub/views/welcome/welcome_view.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MultiProvider(providers: const [], child: const MyApp()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+  await Inicializa.inicializaAplic();
+
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(create: (_) => ProviderWelcomeAuthentication()),
+  ], child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -17,9 +29,10 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        '/': (context) => const HomeView(),
+        '/': (context) => const SplashView(),
+        '/home': (context) => const HomeView(),
+        '/welcome': (context) => WelcomeView(),
       },
-      home: const HomeView(),
     );
   }
 }
