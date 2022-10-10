445c3ac1d96bd14257ae81d71fd11f140fcb0a20
kaiolabs <peypro1@gmail.com>
2022-10-10 00:28:06 -0300

    Adicionando mais itens ao provider

--- lib/controllers/provider_finalize_purchase.dart
+++ lib/controllers/provider_finalize_purchase.dart
@@ -2,43 +2,82 @@ import 'package:flutter/cupertino.dart';
 
 class ProviderfinalizePurchase extends ChangeNotifier {
   int _counter = 1;
-  int _price = 0;
-  int _totalPrice = 0;
+  int _pricePlate = 0;
+  int _totalPricePlate = 0;
+  int _totalPriceBasket = 0;
+  final List<Map> _orderListbasket = [];
 
   int get counter => _counter;
 
-  int get price => _price;
+  int get pricePlate => _pricePlate;
 
-  int get totalPrice => _totalPrice;
+  int get totalPrice => _totalPricePlate;
+
+  List<Map> get orderListbasket => _orderListbasket;
+
+  int get totalPriceBasket => _totalPriceBasket;
 
   init(value) {
-    _price = value;
-    _totalPrice = value;
+    _pricePlate = value;
+    _totalPricePlate = value;
     notifyListeners();
   }
 
   void increment() {
     _counter++;
-    _totalPrice = _price * _counter;
+    _totalPricePlate = _pricePlate * _counter;
     notifyListeners();
   }
 
   void decrement() {
     if (_counter > 1) {
       _counter--;
-      _totalPrice = _totalPrice - _price;
+      _totalPricePlate = _totalPricePlate - _pricePlate;
       notifyListeners();
     }
   }
 
   void setPrice(int price) {
-    _price = price;
+    _pricePlate = price;
     notifyListeners();
   }
 
   void resetAll() {
     _counter = 1;
-    _price = 0;
+    _pricePlate = 0;
+    notifyListeners();
+  }
+
+  void addOrderList(Map order) {
+    _orderListbasket.add(order);
+    notifyListeners();
+  }
+
+  void removeOrderList(int index) {
+    _orderListbasket.removeAt(index);
+    notifyListeners();
+  }
+
+  void resetOrderList() {
+    _orderListbasket.clear();
+    notifyListeners();
+  }
+
+  void setTotal(int value) {
+    _totalPricePlate = value;
+    notifyListeners();
+  }
+
+  void totalPurchaseAmount() {
+    _totalPriceBasket = 0;
+    for (var i = 0; i < _orderListbasket.length; i++) {
+      _totalPriceBasket += _orderListbasket[i]['price'] as int;
+    }
+    notifyListeners();
+  }
+
+  void clearList() {
+    _orderListbasket.clear();
     notifyListeners();
   }
 }
