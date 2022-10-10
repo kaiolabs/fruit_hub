d9a2309dee9727db869fa88a7e83045bdb91e6ff
kaiolabs <peypro1@gmail.com>
2022-10-10 00:30:18 -0300

    Customizando o circular button

--- lib/views/components/circular_button.dart
+++ lib/views/components/circular_button.dart
@@ -2,27 +2,33 @@ import 'package:flutter/material.dart';
 
 class CircularButton extends StatelessWidget {
   final Widget icon;
-  const CircularButton({super.key, required this.icon});
+  final Function()? onTap;
+  const CircularButton({super.key, required this.icon, this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      child: Container(
-        decoration: BoxDecoration(
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            spreadRadius: -3,
+            blurRadius: 8,
+            offset: const Offset(0, 2), // changes position of shadow
+          ),
+        ],
+      ),
+      width: 48,
+      height: 48,
+      child: Material(
+        borderRadius: BorderRadius.circular(25),
+        child: InkWell(
           borderRadius: BorderRadius.circular(25),
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.2),
-              spreadRadius: -3,
-              blurRadius: 8,
-              offset: const Offset(0, 2), // changes position of shadow
-            ),
-          ],
+          onTap: onTap,
+          child: icon,
         ),
-        width: 48,
-        height: 48,
-        child: icon,
       ),
     );
   }
