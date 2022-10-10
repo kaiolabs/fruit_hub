fc16265d4bf3135e9ee93b09a7e6ce16480d57f6
kaiolabs <peypro1@gmail.com>
2022-10-10 00:29:01 -0300

    Acrescentando mais funcionalidades ao button pattern

--- lib/views/components/button_pattern.dart
+++ lib/views/components/button_pattern.dart
@@ -3,8 +3,18 @@ import 'package:fruit_hub/shared/palette.dart';
 
 class ButtonPattern extends StatelessWidget {
   final String lebel;
+  final Color? color;
+  final Color? textColor;
+  final String? fontFamily;
   final Function() onPressed;
-  const ButtonPattern({super.key, required this.lebel, required this.onPressed});
+  const ButtonPattern({
+    super.key,
+    required this.lebel,
+    required this.onPressed,
+    this.color = Palette.orange,
+    this.textColor = Colors.white,
+    this.fontFamily = 'TTNorms_Medium',
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -17,16 +27,16 @@ class ButtonPattern extends StatelessWidget {
             Radius.circular(10),
           ),
         ),
-        color: Palette.orange,
+        color: color,
         child: InkWell(
           onTap: () => onPressed(),
           child: Center(
             child: Text(
               lebel,
-              style: const TextStyle(
+              style: TextStyle(
                 fontSize: 16,
                 fontFamily: 'TTNorms_Medium',
-                color: Color(0xFFFFFFFF),
+                color: textColor,
               ),
             ),
           ),
