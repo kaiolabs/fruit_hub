664687c788d7348c3006ee6c93259aef2c1bdd5b
kaiolabs <peypro1@gmail.com>
2022-10-10 21:52:35 -0300

    Polindo o componente chip_pattern

--- lib/views/components/chip_pattern.dart
+++ lib/views/components/chip_pattern.dart
@@ -7,12 +7,16 @@ class ChipPattern extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.only(right: 8, top: 8, bottom: 8),
+      margin: const EdgeInsets.only(right: 8, top: 4, bottom: 4),
+      decoration: BoxDecoration(
+        border: Border.all(color: const Color(0xFFB4B4C0).withOpacity(0.2)),
+        borderRadius: BorderRadius.circular(10),
+      ),
       alignment: Alignment.center,
       child: Material(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
         elevation: 2,
-        shadowColor: Colors.black.withOpacity(0.4),
+        shadowColor: Colors.black.withOpacity(0.2),
         child: InkWell(
           borderRadius: BorderRadius.circular(10),
           onTap: () {},
